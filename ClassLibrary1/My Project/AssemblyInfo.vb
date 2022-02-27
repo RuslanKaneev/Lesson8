5a3e22a3b32c1cc3da406d01c0d98033e8569ac6
Ruslan <karuslan@inbox.ru>
2022-02-27 23:49:45 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общими сведениями о сборке управляет следующий 
+' набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("ClassLibrary1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ClassLibrary1")> 
+<Assembly: AssemblyCopyright("Copyright ©  2022")> 
+<Assembly: AssemblyTrademark("")> 
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'      Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Сборка: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: ComVisible(false)>
---
+++ Lesson8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Создать консольное приложение, которое при старте выводит приветствие, записанное в настройках приложения (application-scope).
+Запросить у пользователя имя, возраст и род деятельности, а затем сохранить данные в настройках.
+При следующем запуске отобразить эти сведения. Задать приложению версию и описание.*/
+
+namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
